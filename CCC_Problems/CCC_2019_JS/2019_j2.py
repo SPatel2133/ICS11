e43a601b82dea7976b9c9de14daa2d2bd7c57394
Theelunar <jumpscare2133@gmail.com>
2024-10-30 13:17:30 -0400

    Update 2019_j2.py

--- CCC_Problems/CCC_2019_JS/2019_j2.py
+++ CCC_Problems/CCC_2019_JS/2019_j2.py
@@ -1,1 +1,10 @@
-#Time to decrompress "CANNOT SOLVE"
+#Time to decrompress
+to_print = []
+lines = int(input())#number of lines
+for i in range(lines):
+    encoded = input().split()
+    count = int(encoded[0])
+    letter = encoded[1]
+    to_print.append(letter * count)
+for i in to_print:
+    print(i)
