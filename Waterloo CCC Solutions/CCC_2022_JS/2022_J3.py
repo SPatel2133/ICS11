5b39909c8991a51fce48e99c9e0b6a02736eae8a
Theelunar <jumpscare2133@gmail.com>
2025-02-16 23:19:15 -0500

    Update 2022_J3.py

--- Waterloo CCC Solutions/CCC_2022_JS/2022_J3.py
+++ Waterloo CCC Solutions/CCC_2022_JS/2022_J3.py
@@ -1,33 +1,16 @@
-#Harp Tuning
-instructions = input("Enter instructions: ")  # Example: "AFB+8HC-4"
-letters = "QWERTYUIOPASDFGHJKLZXCVBNM"
-numbers = "1234567890"
 
-
-current = ""  # Variable to hold the current segment
+instructions = input("Enter Instructions ")
+current = ""
 
 
 for i in range(len(instructions)):
-    char = instructions[i]
-    
-    # Process letters
-    if char in letters:
-        current += char
-
-
-    # Process '+' or '-'
-    elif char == "+":
-        current += " tighten "
-    elif char == "-":
-        current += " loosen "
-
-
-    # Process numbers
-    elif char in numbers:
-        current += char
-
-
-    # Check if the current segment ends
-    if i + 1 >= len(instructions) or instructions[i + 1] in letters:
-        print(current)  # Print the completed segment
-        current = ""  # Reset for the next segment
+   if instructions[i].isalpha():
+       current += instructions[i]
+   elif instructions[i] == "+":
+       current += " tighten "
+   elif instructions[i] == "-":
+       current += " loosen "
+   elif instructions[i].isdigit():
+       current += instructions[i]
+       print(current)
+       current = ""
